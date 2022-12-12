d2f3a5aab996948142393d326c4e300251ab0d13
Dmitry Delols <dimonsokolov228@mail.ru>
2022-12-12 15:19:06 +0300

    yes

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+//var Coco = make(chan int)
+
+func main() {
+	//var store model.Storage
+	//store = list.NewList()
+	//store.Add(88)
+	//xzc := store.Get(0)
+	//fmt.Println(xzc)
+	//store = &slice.Slice{}
+	//store.Add(22)
+
+	//defer sleeper()
+	//	var mutex sync.Mutex
+	//	for i := 1; i < 4; i++ {
+	//		go sleeper(i, Coco, &mutex)
+	//	}
+	//	<-Coco
+	//	fmt.Println("Функция main отработала")
+	//
+}
+
+//func sleeper(number int, ch chan int, mutex *sync.Mutex) {
+//	mutex.Lock()
+//	time.Sleep(3 * time.Second)
+//	u := number
+//	fmt.Println("Функция sleeper отработала")
+//	mutex.Unlock()
+//	Coco <- u
+//}
